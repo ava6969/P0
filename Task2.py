1faae518278e71780926594e91bd35c3ee930da2
adesola adedewe <ava6969@rit.edu>
2020-08-02 02:24:39 -0400

    finished analysis.txt

--- Task2.py
+++ Task2.py
@@ -20,15 +20,31 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+#
+# tel, _,_, call_time = zip(*calls)
+# dict_ = {}
+#
+# max_ct = 0
+# max_t = ''
+# for t, c in zip(tel, call_time):
+#     if int(c) > max_ct:
+#         max_ct = int(c)
+#         max_t = t
+#
+rec_dict = {}
+for call in calls:
+    if call[1] not in rec_dict:
+        rec_dict.update({call[1]: int(call[3])})
+    else:
+        rec_dict[call[1]] += int(call[3])
 
-tel, _,_, call_time = zip(*calls)
-dict_ = {}
+    if call[0] not in rec_dict:
+        rec_dict.update({call[0]: int(call[3])})
+    else:
+        rec_dict[call[0]] += int(call[3])
 
-max_ct = 0
-max_t = ''
-for t, c in zip(tel, call_time):
-    if int(c) > max_ct:
-        max_ct = int(c)
-        max_t = t
+longest_duration = max(rec_dict.items(), key=lambda x: x[1])
 
-print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(max_t, max_ct))
+print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(*longest_duration))
+
+# print("{} spent the longest time, {} seconds, on the phone during September 2016.".format(max_t, max_ct))
